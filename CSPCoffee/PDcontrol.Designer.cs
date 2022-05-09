9be1a322a538c1a67509a991d25ac862db3ff6db
t816048we@gmail.com <t816048we@gmail.com>
2022-05-09 10:56:14 +0800

    5.9

--- CSPCoffee/PDcontrol.Designer.cs
+++ CSPCoffee/PDcontrol.Designer.cs
@@ -35,6 +35,7 @@ namespace CSPCoffee
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -53,7 +54,7 @@ namespace CSPCoffee
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(0, 170);
+            this.checkBox1.Location = new System.Drawing.Point(-1, 198);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(72, 16);
             this.checkBox1.TabIndex = 6;
@@ -63,27 +64,30 @@ namespace CSPCoffee
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 158);
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(3, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.Size = new System.Drawing.Size(43, 16);
             this.label3.TabIndex = 7;
             this.label3.Text = "label3";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(3, 146);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.Size = new System.Drawing.Size(49, 14);
             this.label2.TabIndex = 4;
             this.label2.Text = "label2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
@@ -95,11 +99,22 @@ namespace CSPCoffee
             this.flowLayoutPanel1.Size = new System.Drawing.Size(194, 28);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(99, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "加入購物車";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // PDcontrol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -123,5 +138,6 @@ namespace CSPCoffee
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- CSPCoffee/PDcontrol.cs
+++ CSPCoffee/PDcontrol.cs
@@ -15,28 +15,28 @@ namespace CSPCoffee
     {
         CoffeeEntities1 db = new CoffeeEntities1();
 
-        public  PDcontrol(int group,int count)
+        public PDcontrol(int productID)
         {
-            
+
             InitializeComponent();
 
 
             //產品名稱
-            var name = db.Products.AsEnumerable().Where(p=>p.CategoryID==group).ToArray();
-            label1.Text = name[count].ProductName.ToString();
+            var name = db.Products.AsEnumerable().Where(p => p.ProductID== productID).ToArray();
+            label1.Text = name[0].ProductName.ToString();
             label1.Click += PDcontrol_Click;
             //產品價格
-            string pric = $"{name[count].Price:c2}元";
+            string pric = $"{name[0].Price:c2}元";
             label2.Text = pric;
             label2.Click += PDcontrol_Click;
             //產品類別
-            var Catergory = db.Products.AsEnumerable().Where(p=>p.CategoryID==group).Select(p => p.Categories.CategoriesName).ToArray();
-            string ctg = $"{Catergory[count]}";
+            var Catergory = db.Products.AsEnumerable().Where(p => p.ProductID == productID).Select(p => p.Categories.CategoriesName).ToArray();
+            string ctg = $"{Catergory[0]}";
             label3.Text = ctg;
             label3.Click += PDcontrol_Click;
             //產品圖片
-            var photo = db.PhotoDetails.AsEnumerable().Select(p => new { p.Products.CategoryID,p.ProductID, p.Photos.Photo }).ToList();
-            byte[] bytes = photo[count].Photo;
+            var photo = db.PhotoDetails.AsEnumerable().Where(p => p.ProductID == productID).Select(p => new { p.ProductID, p.Photos.Photo }).ToList();
+            byte[] bytes = photo[0].Photo;
             MemoryStream ms = new MemoryStream(bytes);
             pictureBox1.Image = Image.FromStream(ms);
             pictureBox1.Click += PDcontrol_Click;
@@ -46,5 +46,6 @@ namespace CSPCoffee
             Productdetail pd = new Productdetail(sender);
             pd.Show();
         }
+
     }
 }
